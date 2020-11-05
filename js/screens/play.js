2008cb585a9c61b73f73ceb798416029a00d7fc8
Delta <abh8210@gmail.com>
2020-11-05 19:38:12 +0530

    revert

--- js/screens/play.js
+++ js/screens/play.js
@@ -109,7 +109,7 @@ game.PlayScreen = me.ScreenObject.extend({
             .easing(me.Tween.Easing.Linear.None)
             .onComplete(function() {
                 game.data.start = true;
-                me.game.world.addChild(new game.ObstacleGenerator(), 0);
+                me.game.world.addChild(new game.PipeGenerator(), 0);
                 me.game.world.removeChild(that.getReady);
             }).start();
         me.game.world.addChild(this.HUD2, 11);
